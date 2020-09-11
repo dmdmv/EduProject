89f7312f80a5114b21cc3229268fa2b15483d0a4
Administrator <Administrator@DESKTOP-D9MC73G>
2020-09-11 16:24:38 +0800

    第一次上传

--- src/com/baidu/aip/auth/FaceMatch.java
+++ src/com/baidu/aip/auth/FaceMatch.java
@@ -11,16 +11,6 @@ import java.util.*;
 * 人脸对比
 */
 public class FaceMatch {
-
-    /**
-    * 重要提示代码中所需工具类
-    * FileUtil,Base64Util,HttpUtil,GsonUtils请从
-    * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
-    * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
-    * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
-    * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3
-    * 下载
-    */
     public static String faceMatch() {
         // 请求url
         String url = "https://aip.baidubce.com/rest/2.0/face/v3/match";
